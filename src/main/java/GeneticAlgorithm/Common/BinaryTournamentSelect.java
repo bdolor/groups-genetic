365fd254be865be42567c3590b8f44f91420b8ab
bdolor <bpayne@bccampus.ca>
2015-11-11 21:17:34 -0800

    modifying tournament selection to maximize tournament pool

--- src/main/java/GeneticAlgorithm/Common/BinaryTournamentSelect.java
+++ src/main/java/GeneticAlgorithm/Common/BinaryTournamentSelect.java
@@ -12,12 +12,17 @@ public class BinaryTournamentSelect<T extends IChromosome> implements ISelect<T>
 	public T[] GetParents(ArrayList<T> population, double[] fitness, int parentCount) {
 		IChromosome[] parents = new IChromosome[parentCount];
 		int pool = fitness.length;
-		int contenders = 20;
+		int contenders;
 		double champ1fitness = 0;
 		double champ2fitness = 0;
 		int redCorner = 0;
 		int blueCorner = 0;
-
+		
+		// ensure that pool tournament selelction is large 
+		// and that it is divisible by 2
+		contenders = ( pool % 2 == 0 ) ? pool : pool - 1;
+		int halfContenders = contenders / 2;
+		
 		// create a list of numbers
 		ArrayList<Integer> randomList = new ArrayList<>();
 		for (int i = 0; i < pool; i++) {
@@ -28,17 +33,17 @@ public class BinaryTournamentSelect<T extends IChromosome> implements ISelect<T>
 		Collections.shuffle(randomList);
 		// choose a number of individuals randomly from a population
 		// repeat until the mating pool is filled
-		for (int i = 0; i < contenders; i++) {
+		for (int i = 0; i < halfContenders; i++) {
 
-			// select the best individuals from the group
+			// select the best individuals from the first half of the pool
 			if (fitness[randomList.get(i)] > champ1fitness) {
 				champ1fitness = fitness[randomList.get(i)];
 				redCorner = randomList.get(i);
 			}
-
-			if (fitness[randomList.get(i + contenders)] > champ2fitness) {
-				champ2fitness = fitness[randomList.get(i + contenders)];
-				blueCorner = randomList.get(i + contenders);
+			// select the best individuals from the last half of the pool
+			if (fitness[randomList.get(i + halfContenders)] > champ2fitness) {
+				champ2fitness = fitness[randomList.get(i + halfContenders)];
+				blueCorner = randomList.get(i + halfContenders);
 
 			}
 		}
